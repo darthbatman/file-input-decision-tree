2e4063bd8a54d1f27bbf1d1607795b6f9531f7c6
Rishi Masand <masandrishi@gmail.com>
2017-08-15 12:16:20 -0400

    commented

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
+from sklearn import tree
+
+# save input file to string
+
 with open("input") as f:
+
 	content = f.readlines()
+
+# remove extra whitespace
 
 content = [x.strip() for x in content]
 
+# read input file
+
 trainingDataLength = 0
-numberOfProperties = 0
 testingDataLength = 0
 
 trainingData = []
@@ -13,38 +21,61 @@ testingData = []
 trainingLabels = []
 
 for i in range(0, len(content)):
+
 	if i == 0:
+
 		trainingDataLength = int(content[i].split(" ")[0])
-		numberOfProperties = int(content[i].split(" ")[1])
+
 		testingDataLength = int(content[i].split(" ")[2])
+
 	elif i <= trainingDataLength:
+
 		trainingData.append(content[i].replace("(", "").replace(")", "").split(","))
+
 	elif i > trainingDataLength and i <= trainingDataLength * 2:
-		trainingLabels.append(nt(content[i]))
+
+		trainingLabels.append(int(content[i]))
+
 	elif i > trainingDataLength * 2:
+
 		testingData.append(content[i].replace("(", "").replace(")", "").split(","))
 
 trainingDataIntegers = []
 
 for i in range(0, len(trainingData)):
+
 	temp = []
+
 	for j in range(0, len(trainingData[i])):
+
 		temp.append(int(trainingData[i][j]))
+
 	trainingDataIntegers.append(temp)
 
 testingDataIntegers = []
 
 for i in range(0, len(testingData)):
+
 	temp = []
+
 	for j in range(0, len(testingData[i])):
+
 		temp.append(int(testingData[i][j]))
+
 	testingDataIntegers.append(temp)
 
-from sklearn import tree
+# creating decision tree
+
 my_classifier = tree.DecisionTreeClassifier()
+
+# training decision tree
 
 my_classifier.fit(trainingDataIntegers, trainingLabels)
 
+# using decision tree to classify testing data
+
 predictions = my_classifier.predict(testingData)
+
 print testingData
+
 print predictions
